04188e7131ad2f732b18cb4c5812af74d3decfcb
orel hassid <orelhassid@outlook.com>
2020-12-02 20:54:01 +0200

    Adding script tag in the head

--- js/theme.js
+++ js/theme.js
@@ -3,4 +3,32 @@
 
 // darkThemeButton.addEventListener("click", setDarkTheme);
 
-__console.environment.ThemeStore.setState({ mode: "dark" });
+function addJS() {
+  let scriptToAdd = document.createElement("script");
+  scriptToAdd.type = "text/javascript";
+
+  // Create contents of the script
+  let inlineScript = document.createTextNode(
+    "__console.environment.ThemeStore.setState({ mode: 'dark' });"
+  );
+
+  scriptToAdd.appendChild(inlineScript);
+
+  // Uncomment to load script from another
+  // source
+  // scriptToAdd.src = 'myscript.js';
+
+  // Get the head element of the document
+  // and append the script
+  document.head.appendChild(scriptToAdd);
+
+  // Update textarea
+  updateHeadOutput();
+}
+
+function updateHeadOutput() {
+  document.querySelector(".head-element").textContent = document.head.innerHTML;
+}
+
+addJS();
+updateHeadOutput();
