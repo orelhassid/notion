8da134afe1584b9b3365496dc5eccb9fbc736ea9
orel hassid <orelhassid@outlook.com>
2021-01-07 15:00:31 +0200

    Remove async script

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  let script = document.querySelector("script"); // Analytics
-  script.remove();
-  console.log("DOM fully loaded and parsed");
+  let scripts = document.querySelectorAll("script"); // Analytics
+  scripts = Array.from(scripts);
+  let anaylitcScript = scripts.find((s) => s.async);
+  anaylitcScript.remove();
 
   const coverImage = document.querySelector(
-    "div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div > img"
+    `div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div > img`
   );
+  console.log("DOM fully loaded and parsed: coverImage", coverImage);
 
   let meta = document.querySelector("meta[property='og:image']");
 
