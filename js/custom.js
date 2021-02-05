771ceb2ed943c2b316a67325b9668152c0af84bd
orel hassid <orelhassid@outlook.com>
2021-02-05 14:11:22 +0200

    document.readyState

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,34 @@
 // Test
 console.log("CREATED BY OREL HASSID");
+if (document.readyState === "complete") {
+  function createCustomButton(label, className) {
+    let contentArea = document.querySelector(".notion-page-content");
+    let button = document.createElement("button");
 
-// Store Variables
-let contentArea = document.querySelector(".notion-page-content");
+    button.innerText = label;
+    button.className = className;
+    button.addEventListener("click", () => {
+      window.location.href = "https://www.yoursite.com";
+    });
 
-function createCustomButton(label, className) {
-  let button = document.createElement("button");
+    contentArea.append(newButton);
+    return button;
+  }
 
-  button.innerText = label;
-  button.className = className;
-  button.addEventListener("click", () => {
-    window.location.href = "https://www.yoursite.com";
-  });
+  createCustomButton("CLICK ON ME", "custom-button");
 
-  contentArea.append(newButton);
-  return button;
+  function setCustomIcons(iconUrl) {
+    let shortcutIcon = document.querySelector("link[rel='shortcut icon']");
+    let appleIcon = document.querySelector("link[rel='apple-touch-icon']");
+    let metaTwitter = document.querySelector("meta[name='twitter:image']");
+    let metaOg = document.querySelector("meta[property='og:image']");
+
+    shortcutIcon.rel = iconUrl;
+    appleIcon.rel = iconUrl;
+    metaTwitter.name = iconUrl;
+    metaOg.property = iconUrl;
+  }
+  setCustomIcons(
+    "https://www.flaticon.com/svg/vstatic/svg/564/564419.svg?token=exp=1612527261~hmac=4715ce4d5a25326b53448f92071da4e5"
+  );
 }
-
-createCustomButton("CLICK ON ME", "custom-button");
-
-function setCustomIcons(iconUrl) {
-  let shortcutIcon = document.querySelector("link[rel='shortcut icon']");
-  let appleIcon = document.querySelector("link[rel='apple-touch-icon']");
-  let metaTwitter = document.querySelector("meta[name='twitter:image']");
-  let metaOg = document.querySelector("meta[property='og:image']");
-
-  shortcutIcon.rel = iconUrl;
-  appleIcon.rel = iconUrl;
-  metaTwitter.name = iconUrl;
-  metaOg.property = iconUrl;
-}
-setCustomIcons(
-  "https://www.flaticon.com/svg/vstatic/svg/564/564419.svg?token=exp=1612527261~hmac=4715ce4d5a25326b53448f92071da4e5"
-);
