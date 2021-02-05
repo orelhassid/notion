abb51d262fc9f8ced0aedf5b5a8612223c449d6e
orel hassid <orelhassid@outlook.com>
2021-02-05 13:14:51 +0200

    update index.js

---
+++ js/index copy.js
@@ -0,0 +1,15 @@
+// if (document.readyState === "complete") {
+//   let scripts = document.querySelectorAll("script"); // Analytics
+//   scripts = Array.from(scripts);
+//   let anaylitcScript = scripts.find((s) => s.async);
+//   anaylitcScript.remove();
+
+//   const coverImage = document.querySelector(
+//     `div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div > img`
+//   );
+//   console.log("DOM fully loaded and parsed: coverImage", coverImage);
+
+//   let meta = document.querySelector("meta[property='og:image']");
+
+//   meta.content = coverImage.src;
+// }
--- js/index.js
+++ js/index.js
@@ -1,15 +1,2 @@
-if (document.readyState === "complete") {
-  let scripts = document.querySelectorAll("script"); // Analytics
-  scripts = Array.from(scripts);
-  let anaylitcScript = scripts.find((s) => s.async);
-  anaylitcScript.remove();
-
-  const coverImage = document.querySelector(
-    `div.notion-frame > div.notion-scroller.vertical.horizontal > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div > img`
-  );
-  console.log("DOM fully loaded and parsed: coverImage", coverImage);
-
-  let meta = document.querySelector("meta[property='og:image']");
-
-  meta.content = coverImage.src;
-}
+// Test
+console.log("CREATED BY OREL HASSID");
