a6c2a6c97d90ad40fe12b1faca51d074c1eb4f24
orel hassid <orelhassid@outlook.com>
2021-02-05 13:33:29 +0200

    Create Custom Button

--- js/index.js
+++ js/index.js
@@ -1,2 +1,17 @@
 // Test
 console.log("CREATED BY OREL HASSID");
+
+// Store Variables
+let contentArea = document.querySelector(".notion-page-content");
+
+function createCustomButton() {
+  let button = document.createElement("button");
+
+  button.innerText = "CLICK ON ME";
+  button.addEventListener("click", () => {
+    window.location.href = "www.yoursite.com";
+  });
+
+  contentArea.append(newButton);
+  return button;
+}
