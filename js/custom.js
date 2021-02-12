8614fbfcb9f77ed90ec1fb76203c480c3879d803
orel hassid <orelhassid@outlook.com>
2021-02-12 07:01:47 +0200

    Using MutationObserver

--- js/custom.js
+++ js/custom.js
@@ -1,39 +1,37 @@
 // Test
 console.log("CREATED BY OREL HASSID");
-window.addEventListener("DOMContentLoaded", (event) => {
-  if (
-    document.readyState === "interactive" ||
-    document.readyState === "complete"
-  ) {
-    function setCustomIcons(iconUrl) {
-      let shortcutIcon = document.querySelector("link[rel='shortcut icon']");
-      let appleIcon = document.querySelector("link[rel='apple-touch-icon']");
-      let metaTwitter = document.querySelector("meta[name='twitter:image']");
-      let metaOg = document.querySelector("meta[property='og:image']");
 
-      shortcutIcon.href = iconUrl;
-      appleIcon.href = iconUrl;
-      metaTwitter.content = iconUrl;
-      metaOg.content = iconUrl;
-    }
+function setCustomIcons(iconUrl) {
+  let shortcutIcon = document.querySelector("link[rel='shortcut icon']");
+  let appleIcon = document.querySelector("link[rel='apple-touch-icon']");
+  let metaTwitter = document.querySelector("meta[name='twitter:image']");
+  let metaOg = document.querySelector("meta[property='og:image']");
 
-    function setClassNames() {
-      let pageTitleBlock = document.querySelector(
-        "div[placeholder='Untitled']"
-      );
-      let pageFrame = document.querySelector(".notion-frame");
-      const pageTitle = pageTitleBlock.textContent;
+  shortcutIcon.href = iconUrl;
+  appleIcon.href = iconUrl;
+  metaTwitter.content = iconUrl;
+  metaOg.content = iconUrl;
+}
 
-      pageFrame.id = pageTitle; // Set Notion Frame ID to page title
-    }
+function setClassNames() {
+  let pageTitleBlock = document.querySelector("div[placeholder='Untitled']");
+  let pageFrame = document.querySelector(".notion-frame");
+  const pageTitle = pageTitleBlock.textContent;
 
-    function init() {
-      setCustomIcons(
-        "https://raw.githubusercontent.com/orelhassid/notion/b3768a3322588ba0bb1b913abae02d3e7a54b2bb/icons/logo.svg"
-      );
-      setClassNames();
-    }
+  pageFrame.id = pageTitle; // Set Notion Frame ID to page title
+}
 
-    setTimeout(init, 3000);
-  }
+const observer = new MutationObserver(function () {
+  const app = document.querySelector("#notion-app");
+  if (!app) return;
+
+  setCustomIcons(
+    "https://raw.githubusercontent.com/orelhassid/notion/b3768a3322588ba0bb1b913abae02d3e7a54b2bb/icons/logo.svg"
+  );
+  setClassNames();
+});
+
+observer.observe(document.querySelector("#notion-app"), {
+  childList: true,
+  subtree: true,
 });
