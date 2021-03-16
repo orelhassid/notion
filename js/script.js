0629495e6a28dd09f8dc70d2c998fd8e5121a358
orel hassid <orelhassid@outlook.com>
2021-03-16 23:15:36 +0200

    add rtl style

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 console.log("Index.js File. Import CSS Files");
+
+const gitHubUrl = "https://orelhassid.github.io/notion";
 function addStyle(href, rel) {
   let linkToAdd = document.createElement("link");
   linkToAdd.href = href;
@@ -7,4 +9,5 @@ function addStyle(href, rel) {
   document.head.appendChild(linkToAdd);
 }
 
-addStyle(`https://orelhassid.github.io/notion/css/theme.css`);
+addStyle(`${gitHubUrl}/css/theme.css`);
+addStyle(`${gitHubUrl}/css/rtl.css`);
